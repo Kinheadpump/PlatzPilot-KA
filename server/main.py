4827c6aab490a43ef179a9c403cf8dca188a7e5d
Kinheadpump <67102569+Kinheadpump@users.noreply.github.com>
2025-08-05 19:32:09 +0200

    Incorporated Forecasting Model

--- server/main.py
+++ server/main.py
@@ -6,7 +6,7 @@ from tools.log import setup_logger
 from tools.fetcher import fetch_seats
 from tools.storage import RingBufferStore
 from tools.formatting import json_handler
-
+from tools.forecast import ForecastManager
 
 SEATS_URL = "https://seatfinder.bibliothek.kit.edu/karlsruhe/getdata.php?callback=jQuery37101524490458818586_1753302096731&location%5B0%5D=LSG%2CLSM%2CLST%2CLSN%2CLSW%2CLBS%2CBIB-N%2CL3%2CL2%2CSAR%2CL1%2CLEG%2CFBC%2CFBP%2CLAF%2CFBA%2CFBI%2CFBM%2CFBH%2CFBD%2CBLB%2CWIS&values%5B0%5D=seatestimate%2Cmanualcount&after%5B0%5D=-10800seconds&before%5B0%5D=now&limit%5B0%5D=-17&location%5B1%5D=LSG%2CLSM%2CLST%2CLSN%2CLSW%2CLBS%2CBIB-N%2CL3%2CL2%2CSAR%2CL1%2CLEG%2CFBC%2CFBP%2CLAF%2CFBA%2CFBI%2CFBM%2CFBH%2CFBD%2CBLB%2CWIS&values%5B1%5D=location&after%5B1%5D=&before%5B1%5D=now&limit%5B1%5D=1&_=1753302096732"
 LOCATION_NUMBER = 22
@@ -15,9 +15,17 @@ FETCH_INTERVAL = 300
 log_file = "log"
 ring_buffer_save_file = "data"
 json_save_file = "seat_finder_data.json"
+forecast_model_dir = "model_states"
 
 ring_buffer = RingBufferStore(storage_dir=ring_buffer_save_file)
 logger = setup_logger(name="seat_tracker", level=logging.DEBUG, logger_dir=log_file)
+
+forecast_manager = ForecastManager(
+    ring_buffer,
+    model_dir=forecast_model_dir,
+    num_buildings=22,
+    season_length=288
+)
 
 
 def main():
@@ -60,7 +68,7 @@ def main():
                 logger.critical("number_of_free_seats_currently not the expected length")
                 raise ValueError("Expected number_of_free_seats_currently to be a list of length %s", LOCATION_NUMBER)
 
-            ring_buffer.append(number_of_free_seats_currently)
+            ring_buffer.append(number_of_free_seats_currently, last_seat_count_update)
             logger.info("Appended %d-seat record at buffer pos %d", len(seat_estimate), ring_buffer.pointer)
 
             location = fetched_data[1]['location']
@@ -69,7 +77,10 @@ def main():
                 raise ValueError("Expected fetched_data[0]['location'] to be a dictionary of length %s",
                                  LOCATION_NUMBER)
 
-            json_to_push = json_handler(location, number_of_free_seats_currently, library_is_closed_flag)
+            forecasts = forecast_manager.update_and_forecast()
+            logger.debug("forecast returned: %s", forecasts)
+
+            json_to_push = json_handler(location, forecasts, number_of_free_seats_currently, library_is_closed_flag)
             logger.debug("json_handler returned: %s", json_to_push)
 
             with open(json_save_file, 'w') as f:
